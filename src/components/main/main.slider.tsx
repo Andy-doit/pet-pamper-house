fdf329569f024e99614a0ee0dfea14daa2a58b5a
ThienAnDy <vaper2212@gmail.com>
2024-11-06 10:34:42 +0700

    update

--- src/components/main/main.slider.tsx
+++ src/components/main/main.slider.tsx
@@ -2,8 +2,6 @@
 import Slider from "react-slick";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
-import { Button } from "@nextui-org/react";
-import { FaArrowAltCircleRight } from "react-icons/fa";
 
 
 const MainSlider = () => {
